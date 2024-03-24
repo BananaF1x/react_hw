95cc0033077afc3082ba0f659b92a3272e466019
BananaF1x <brovkoman@gmail.com>
2024-03-24 11:13:14 +0200

    added home work 42

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,14 @@
+import  Button from '../Button/Button'
+import  Input from '../Input/Input'
+
+const Form = () => {
+
+  return (
+   <form className='login-form'>
+    <Input />
+    <Button />
+   </form> 
+  )
+}
+
+export default Form;
